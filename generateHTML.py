226d6d72aa359287f450e7a63cbe9c3ca5c149c5
Engel, Ryan (re153b) <re153b@att.com>
2019-12-19 14:52:22 -0600

    Initial Commit

---
+++ generateHTML.py
@@ -0,0 +1,23 @@
+stop = 0
+html = ""
+while(True):
+	headNum = input("Header Num: ")
+	if(headNum == "end"):
+		break
+	headTitle = input("Header Title: ")
+	text = ""
+	while(True):
+		temp = input("body: ")
+		if((temp != "end") and (temp != "")):
+			text = text + "<p>&emsp;" + temp + "</p>\n"
+		else:
+			if(temp == "end"):
+				break
+	html = html + "\n<h" + str(int(headNum)+1) + ">" + headTitle + "</h" + str(int(headNum)+1) + ">\n"
+	html = html + text
+		
+	
+html = html.encode('ascii', 'ignore')
+fp = open("htmlOut.txt", 'wb')
+fp.write(html)
+# print(html[20250:20265])
